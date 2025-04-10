dee29e57b85af44a70f3a4426853fa6ce51075b0
Unchpokable <Unchpokable@outlook.com>
2025-04-11 00:33:53 +0500

    eh

--- bot_runtime.hxx
+++ bot_runtime.hxx
@@ -1,21 +1,26 @@
 #pragma once
 
 #include <tgbot/tgbot.h>
-#include <queue>
+
+namespace lua {
+struct LuaScript;
+}
 
 namespace tg {
 
-struct BotConfig {
-    
-};
-
 class BotRuntimeContext {
-
 public:
+    
 
 private:
+    void toKeys(std::vector<lua::LuaScript*> scripts);
+
+    std::unordered_map<std::string, lua::LuaScript*> _commands;
+
     std::mutex _mutex;
     std::thread _botThread;
+
+    std::vector<uint64_t> _maintainers;
 };
 
 }
